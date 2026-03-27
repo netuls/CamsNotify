2f0ba44c6c4bf74a4ed004bed2e0a5506ac93bf1
netuls <56983125+netuls@users.noreply.github.com>
2026-03-26 22:07:41 -0300

    Update and rename camsnotify-backend to CamsNotify / camsnotify-backend /server.js

--- CamsNotify / camsnotify-backend /server.js
+++ CamsNotify / camsnotify-backend /server.js
@@ -147,16 +147,3 @@ cron.schedule('* * * * *', async () => {
 });
 
 app.listen(PORT, () => console.log(`🌸 CamsNotify Backend rodando na porta ${PORT}`));
-
-{
-  "name": "camsnotify-backend",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC"
-}
